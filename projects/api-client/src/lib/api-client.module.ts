a5edc8ed6e3bcef0d32f718ee50fb11c61227da5
Eduardo Juliao <eduardo.juliao@kneat.com>
2019-03-14 15:38:47 +0000

    ApiClient

---
+++ projects/api-client/src/lib/api-client.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ApiClientComponent } from './api-client.component';
+
+@NgModule({
+  declarations: [ApiClientComponent],
+  imports: [
+  ],
+  exports: [ApiClientComponent]
+})
+export class ApiClientModule { }
